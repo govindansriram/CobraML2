b054a28a40b73bd43b910807d3336b0d7833194e
root <sriramgovindanwork@gmail.com>
2026-01-02 09:09:41 +0000

    checkpoint

--- tests/test_fmha_cc.cu
+++ tests/test_fmha_cc.cu
@@ -9,16 +9,13 @@
 using namespace cobraml;
 using namespace cute;
 
-TEST(MHA_TEST, kernel) {
-
-    constexpr int head_count{16};
-    constexpr int head_dim{64};
-    constexpr int B_r{64};
-    constexpr int B_c{64};
-
-    int batch_size{56};
-    int sequence_length{128};
-
+template<
+    int head_count,
+    int head_dim,
+    int B_r,
+    int B_c
+>
+void test_fmha(int batch_size, int sequence_length){
     using MHAType = kernels::FMHA<head_count, head_dim, B_r, B_c, float>;
 
     thrust::device_vector<float> q_device{
@@ -28,41 +25,45 @@ TEST(MHA_TEST, kernel) {
         )
     };
 
-    // thrust::device_vector<float> k_device{
-    //     create_projection<float>(
-    //         head_count, head_dim, batch_size, sequence_length,
-    //         seeded_fill_fn(1)
-    //     )
-    // };
+    thrust::device_vector<float> k_device{
+        test_helpers::create_projection<float>(
+            head_count, head_dim, batch_size, sequence_length,
+            test_helpers::seeded_fill_random_uniform<float>(1)
+        )
+    };
 
-    // thrust::device_vector<float> v_device{
-    //     create_projection<float>(
-    //         head_count, head_dim, batch_size, sequence_length,
-    //         seeded_fill_fn(2)
-    //     )
-    // };
+    thrust::device_vector<float> v_device{
+        test_helpers::create_projection<float>(
+            head_count, head_dim, batch_size, sequence_length,
+            test_helpers::seeded_fill_random_uniform<float>(2)
+        )
+    };
 
-    // auto fill_zero_ptr{fill_zero<float>};
+    thrust::device_vector<float> o_device{
+        test_helpers::create_projection<float>(
+            head_count, head_dim, batch_size, sequence_length,
+            test_helpers::fill_zero<float>
+        )
+    };
 
-    // thrust::device_vector<float> o_device{
-    //     create_projection<float>(
-    //         head_count, head_dim, batch_size, sequence_length,
-    //         fill_zero_ptr
-    //     )
-    // };
+    MHAType mha{};
 
-    // MHAType mha{};
-
-    // mha(
-    //     thrust::raw_pointer_cast(q_device.data()), 
-    //     thrust::raw_pointer_cast(k_device.data()), 
-    //     thrust::raw_pointer_cast(v_device.data()), 
-    //     thrust::raw_pointer_cast(o_device.data()), 
-    //     batch_size, 
-    //     sequence_length
-    // );
+    mha(
+        thrust::raw_pointer_cast(q_device.data()), 
+        thrust::raw_pointer_cast(k_device.data()), 
+        thrust::raw_pointer_cast(v_device.data()), 
+        thrust::raw_pointer_cast(o_device.data()), 
+        batch_size, 
+        sequence_length
+    );
 
     cudaDeviceSynchronize();
     cudaError_t err = cudaGetLastError();
     ASSERT_EQ(err, cudaSuccess) << cudaGetErrorString(err);
+}
+
+TEST(FMHA_CC, H16_Hd64_Br64_Bc64_Bs56_Sq128) {
+    test_fmha<16, 64, 64, 64>(
+        56, 128
+    );
 }

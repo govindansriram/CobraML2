cc8130b651af8d3952ce4d276be559a92cf41a15
Sriram Govindan <62492215+govindansriram@users.noreply.github.com>
2026-01-03 16:04:06 -0800

    Linting (#7)
    * fixed import issues
    
    * added CI test

--- include/cobraml2/algos.cuh
+++ include/cobraml2/algos.cuh
@@ -1,24 +1,22 @@
 #pragma once
+#include <cuda/std/functional>
 #include <cute/config.hpp>
 
-namespace cobraml{
+namespace cobraml {
 
-
-template <typename DType>
-__device__ DType warp_max(DType val) {
-    CUTE_UNROLL
-    for (int offset = 16; offset > 0; offset /= 2)
-        val = cuda::std::max(val, __shfl_xor_sync(0xffffffff, val, offset));
-    return val;
+template <typename DType> __device__ DType warp_max(DType val) {
+  CUTE_UNROLL
+  for (int offset = 16; offset > 0; offset /= 2)
+    val = cuda::std::max(val, __shfl_xor_sync(0xffffffff, val, offset));
+  return val;
 }
 
-template <typename DType>
-__device__ DType warp_sum(DType val) {
+template <typename DType> __device__ DType warp_sum(DType val) {
 
-    CUTE_UNROLL
-    for (int offset = 16; offset > 0; offset /= 2)
-        val += __shfl_xor_sync(0xffffffff, val, offset);
-    return val;
+  CUTE_UNROLL
+  for (int offset = 16; offset > 0; offset /= 2)
+    val += __shfl_xor_sync(0xffffffff, val, offset);
+  return val;
 }
 
-}
+} // namespace cobraml

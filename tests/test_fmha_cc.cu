b590c8337782d4d788bc33b06ba2e0a6edc5dd80
govindansriram <sriramgovindanwork@gmail.com>
2026-01-03 18:35:35 -0800

    added benchmarking using chrono

--- tests/test_fmha_cc.cu
+++ tests/test_fmha_cc.cu
@@ -29,14 +29,34 @@ void test_fmha(int batch_size, int sequence_length) {
 
   MHAType mha{};
 
-  mha(thrust::raw_pointer_cast(q_device.data()),
+  constexpr size_t warmup_iters{2};
+  constexpr size_t total_iters{10};
+  for (size_t i{0}; i < warmup_iters; ++i){
+      mha(thrust::raw_pointer_cast(q_device.data()),
       thrust::raw_pointer_cast(k_device.data()),
       thrust::raw_pointer_cast(v_device.data()),
       thrust::raw_pointer_cast(o_device.data()), batch_size, sequence_length);
-
+  }
   cudaDeviceSynchronize();
+
+  auto start = std::chrono::high_resolution_clock::now();
+  for (size_t i{0}; i < total_iters; ++i){
+      mha(thrust::raw_pointer_cast(q_device.data()),
+      thrust::raw_pointer_cast(k_device.data()),
+      thrust::raw_pointer_cast(v_device.data()),
+      thrust::raw_pointer_cast(o_device.data()), batch_size, sequence_length);
+  }
+  cudaDeviceSynchronize();
+  auto end = std::chrono::high_resolution_clock::now();
+
   cudaError_t err = cudaGetLastError();
   ASSERT_EQ(err, cudaSuccess) << "CUDA error: " << cudaGetErrorString(err);
+
+  std::chrono::duration<double> diff = end - start;
+  double useconds = diff.count() * 1e6 / total_iters;
+  
+  // Calculate TFLOPs
+  std::cout << "Avg Kernel execution time: " << useconds << " us\n";
 
   thrust::host_vector<float> q_host = q_device;
   thrust::host_vector<float> k_host = k_device;
--- tests/test_mha.cu
+++ tests/test_mha.cu
@@ -24,24 +24,47 @@ void test_mha(int batch_size, int sequence_length) {
   thrust::device_vector<float> o_device{test_helpers::create_projection<float>(
       head_count, head_dim, batch_size, sequence_length,
       test_helpers::fill_zero<float>)};
-
-  // Run the naive MHA forward pass
-  // Signature: mha_forward(Q, K, V, O, B, H, N, d)
-  kernels::mha_forward(thrust::raw_pointer_cast(q_device.data()),
-                       thrust::raw_pointer_cast(k_device.data()),
-                       thrust::raw_pointer_cast(v_device.data()),
-                       thrust::raw_pointer_cast(o_device.data()),
-                       batch_size,      // B
-                       head_count,      // H
-                       sequence_length, // N
-                       head_dim         // d
-  );
+      
+  constexpr size_t warmup_iters{2};
+  constexpr size_t total_iters{10};
+  for (size_t i{0}; i < warmup_iters; ++i){
+    kernels::mha_forward(thrust::raw_pointer_cast(q_device.data()),
+                        thrust::raw_pointer_cast(k_device.data()),
+                        thrust::raw_pointer_cast(v_device.data()),
+                        thrust::raw_pointer_cast(o_device.data()),
+                        batch_size,      // B
+                        head_count,      // H
+                        sequence_length, // N
+                        head_dim         // d
+    );
+  }
 
   // Sync and check for CUDA errors
   cudaDeviceSynchronize();
+  auto start = std::chrono::high_resolution_clock::now();
+  for (size_t i{0}; i < total_iters; ++i){
+    kernels::mha_forward(thrust::raw_pointer_cast(q_device.data()),
+                        thrust::raw_pointer_cast(k_device.data()),
+                        thrust::raw_pointer_cast(v_device.data()),
+                        thrust::raw_pointer_cast(o_device.data()),
+                        batch_size,      // B
+                        head_count,      // H
+                        sequence_length, // N
+                        head_dim         // d
+    );
+  }
+  cudaDeviceSynchronize();
+  auto end = std::chrono::high_resolution_clock::now();
+
   cudaError_t err = cudaGetLastError();
   ASSERT_EQ(err, cudaSuccess) << "CUDA error: " << cudaGetErrorString(err);
+
+  std::chrono::duration<double> diff = end - start;
+  double useconds = diff.count() * 1e6 / total_iters;
   // Copy inputs to host for CPU reference
+
+  // Calculate TFLOPs
+  std::cout << "Avg Kernel execution time: " << useconds << " us\n";
 
   thrust::host_vector<float> q_host = q_device;
   thrust::host_vector<float> k_host = k_device;
